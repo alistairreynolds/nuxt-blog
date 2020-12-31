9dbbca83ec70063399ba4a1f2f61cc0d20e92f6f
Alistair Reynolds <alistair@thovex.co.uk>
2020-12-31 17:09:36 +0000

    Add PostPreview component

---
+++ components/PostPreview.vue
@@ -0,0 +1,76 @@
+<template>
+  <nuxt-link :to="'/posts/' + this.id">
+    <article class="post-preview">
+      <div class="post-thumbnail" :style="{backgroundImage:`url('/images/${thumb}')`}" />
+      <div class="post-content">
+        <h2>{{ title }}</h2>
+        <p>{{ text }}</p>
+      </div>
+    </article>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  name: 'PostPreview',
+  props: {
+    id: {
+      type: Number,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    thumb: {
+      type: String,
+      required: true
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  padding: 10px;
+  text-align: center;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
+}
+
+</style>
