63bd940956cde6cc06399035daa7febeeadf3ee4
Alistair Reynolds <alistair@thovex.co.uk>
2020-12-31 18:22:02 +0000

    Add nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,31 @@
 <template>
   <div>
     <div id="app">
+      <TheHeader @sidenavToggle=" displaySideNav = !displaySideNav" />
+      <TheSidenav
+        :show="displaySideNav"
+        @close="displaySideNav = false"
+      />
       <Nuxt />
     </div>
   </div>
 </template>
+
+<script>
+
+import TheHeader from '~/components/Navigation/TheHeader'
+import TheSidenav from '~/components/Navigation/TheSidenav'
+
+export default {
+  components: {
+    TheHeader,
+    TheSidenav
+  },
+  data () {
+    return {
+      displaySideNav: false
+    }
+  }
+}
+
+</script>
