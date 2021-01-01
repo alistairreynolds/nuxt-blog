bb44462482f33a043bec3d33490d06ec9a16be41
Alistair Reynolds <alistair@thovex.co.uk>
2021-01-01 17:01:36 +0000

    Adding post content

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -2,17 +2,19 @@
   <div class="single-post-page">
     <section class="post">
       <h1 class="post-title">
-        Title
+        {{ post.title }}
       </h1>
       <div class="post-details">
-        <div class="post-details">
-          Date:
+        <div class="post-detail">
+          {{ post.updatedDate }}
         </div>
-        <div class="post-details">
-          By:
+        <div class="post-detail">
+          Written By: {{ post.author }}
         </div>
       </div>
-      <p>Contents</p>
+      <p class="post-content">
+        {{ post.content }}
+      </p>
     </section>
 
     <section class="post-feedback">
@@ -22,8 +24,24 @@
 </template>
 
 <script>
+
+import moment from 'moment'
+
 export default {
-  name: 'Post'
+  name: 'Post',
+  asyncData (context, callback) {
+    callback(null, {
+      post: {
+        id: 1,
+        title: 'Some stuff',
+        previewText: 'Makin some stuff',
+        thumb: '/images/post1.jpg',
+        author: 'Me',
+        updatedDate: moment().format('dddd Do MMMM YYYY, h:mm a'),
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      }
+    })
+  }
 }
 </script>
 
@@ -59,10 +77,14 @@ export default {
         flex-direction: row;
       }
 
-      .post-details {
+      .post-detail {
         color: rgb(88, 88, 88);
         margin: 0 10px;
       }
+    }
+
+    .post-content{
+      margin: 30px 0;
     }
 
     .post-feedback a {
