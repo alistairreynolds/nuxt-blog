e0eaa139a3138bda71af4089378e7ce19b150da7
Alistair Reynolds <alistair@thovex.co.uk>
2021-01-01 17:17:31 +0000

    Add the state store

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -7,23 +7,30 @@
 <script>
 export default {
   name: 'PostsIndex',
-  asyncData (context, callback) {
-    callback(null, {
-      posts: [
-        {
-          id: 1,
-          title: 'Some stuff',
-          previewText: 'Makin some stuff',
-          thumb: 'post1.jpg'
-        },
-        {
-          id: 2,
-          title: 'Some other stuff',
-          previewText: 'Makin more stuff',
-          thumb: 'post2.jpg'
-        }
-      ]
+  asyncData (context) {
+    return new Promise((resolve, reject) => {
+      resolve({
+        posts: [
+          {
+            id: 1,
+            title: 'Some stuff',
+            previewText: 'Makin some stuff',
+            thumb: 'post1.jpg'
+          },
+          {
+            id: 2,
+            title: 'Some other stuff',
+            previewText: 'Makin more stuff',
+            thumb: 'post2.jpg'
+          }
+        ]
+      }).then((responseData) => {
+        return { responseData }
+      })
     })
+  },
+  created () {
+    this.$store.dispatch('setPosts', this.posts)
   }
 }
 </script>
